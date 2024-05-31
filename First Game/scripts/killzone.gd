c5d11bf88b9008a20114e1487006d8eb675f5050
oBlacksheep <kamaliasofeakamaludin@gmail.com>
2024-05-31 23:59:17 +0800

    TASK 3
    i actually added the music in file so pls open file manager, but idk why it doesnt appear

--- First Game/scripts/killzone.gd
+++ First Game/scripts/killzone.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @onready var timer = $Timer
 
-func _on_body_entered(body):
+func _on_body_entered(body): 
 	print("You died!")
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
--- First Game/scripts/player.gd
+++ First Game/scripts/player.gd
@@ -1,13 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -500.0
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var sfx_jump = $sfx_jump 
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,6 +18,7 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		sfx_jump.play()
 
 	# Get the input direction: -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
@@ -34,8 +36,8 @@ func _physics_process(delta):
 		else:
 			animated_sprite.play("run")
 	else:
-		animated_sprite.play("jump")
-	
+		animated_sprite.play("jump") 
+		
 	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
